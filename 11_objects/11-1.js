cb715c88444b30da24131c2b3c24150be9fc25e0
Majlee888 <99806034+Majlee888@users.noreply.github.com>
2024-02-11 21:12:04 +0700

    11.1 task

---
+++ 11_objects/11-1.js
@@ -0,0 +1,27 @@
+//1.1 Даны переменные. На их основе создать объект.
+//const firstName = "Иван";
+//const lastName = "Иванов";
+//const age = 23;
+//const email = "ivanov@mail.ru";
+
+//output
+//const user = {
+//"first-name": "Иван",
+//"last-name": "Иванов",
+//age: 23,
+//email: "ivanov@mail.ru",
+//};
+
+const firstName = "Иван";
+const lastName = "Иванов";
+const age = 23;
+const email = "ivanov@mail.ru";
+
+const user = {
+    "first-name": firstName,
+    "last-name": lastName,
+    age: age,
+    email: email,
+
+}
+console.log(user);
